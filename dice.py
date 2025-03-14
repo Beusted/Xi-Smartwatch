4cf00a244c4db44a1196ff53bde8512827707f73
Jesus Moreno <jesusmoreno9418@csu.fullerton.edu>
2025-03-14 16:26:55 -0700

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,8 +1,8 @@
-import random
+import random # imports functions necessary for code
 
-user_choice = int(input("Pick number of faces: "))
+user_choice = int(input("Pick number of faces: ")) # Watch asks for user's input by initializing as only intergers
 
 
-random_number = random.randint(1, user_choice)
+random_number = random.randint(1, user_choice)  # Watch chooses random number between 1 and user's input 
 
-print("Random Number:", random_number)
+print("Random Number:", random_number) # Watch prints number 
