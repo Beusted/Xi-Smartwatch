ea089eb5e59a4c420a43f0f3f7a8c48424bfe98a
Jesus <jesusmoreno9418@csu.fullerton.edu>
2025-03-17 17:17:48 -0700

    change

--- dice.py
+++ dice.py
@@ -6,3 +6,5 @@ user_choice = int(input("Pick number of faces: ")) # Watch asks for user's input
 random_number = random.randint(1, user_choice)  # Watch chooses random number between 1 and user's input 
 
 print("Random Number:", random_number) # Watch prints number 
+
+# Things to add; gif animation, more than one dice a session, pvp 21 game where two players can roll up to 21 to win but not pass it.
