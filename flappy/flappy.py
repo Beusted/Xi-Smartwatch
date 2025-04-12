9dd6ec67465c3070b0c3183e1b8a900e548c559c
dgianna707 <142849135+dgianna707@users.noreply.github.com>
2025-04-11 20:46:35 -0700

    Update flappy.py

--- flappy/flappy.py
+++ flappy/flappy.py
@@ -6,6 +6,7 @@ pygame.init()
 clock = pygame.time.Clock()
 
 # Screen dimensions
+# actual: w: 480, h: 320 make sure we change it | Falsy play around: 720x551
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 320
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -21,64 +22,79 @@ BASE = (107, 106, 105)
 LIGHT_GRAY = (217, 217, 217)
 
 # Image Assets
-pony_images = [pygame.image.load("flappy/assets/pony_stretch.png"), pygame.image.load("flappy/assets/pony_mid.png"), pygame.image.load("flappy/assets/pony_close.png")]
+bird_images = [pygame.image.load("flappy/assets/bird_down.png"), pygame.image.load("flappy/assets/bird_mid.png"), pygame.image.load("flappy/assets/bird_up.png")]
 skyline_image = pygame.image.load("flappy/assets/background.png")
 ground_image = pygame.image.load("flappy/assets/ground.png")
 top_pipe_image = pygame.image.load("flappy/assets/pipe_top.png")
 bottom_pipe_image = pygame.image.load("flappy/assets/pipe_bottom.png")
-game_over_image = pygame.image.load("flappy/assets/game_over.png")
-start_image = pygame.image.load("flappy/assets/start.png")
+game_over_image = pygame.image.load("flappy/assets/GameOver.png")
+start_image = pygame.image.load("flappy/assets/GoldenPonyStartHigher.png")
 
 # Game
 scroll_speed = 1
-pony_start_position = (100, 160)
-score = 0 
+bird_start_position = (100, 160)
+score = 0
+font = pygame.font.SysFont('Segoe', 26)
+game_stopped = True
 
-class Pony(pygame.sprite.Sprite):
+class Bird(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pony_images[0]
+        self.image = bird_images[0]
         self.rect = self.image.get_rect()
-        self.rect.center = pony_start_position
+        self.rect.center = bird_start_position
         self.image_index = 0
         self.vel = 0
         self.flap = False
+        self.alive = True
     
     def update(self):
-        # Pony Animation
+        # Bird Animation
         self.image_index += 1
         if self.image_index >= 30:
             self.image_index = 0
-        self.image = pony_images[self.image_index // 10]
+        self.image = bird_images[self.image_index // 10]
 
         # Gravity and Flap
         self.vel += 0.5
         if self.vel > 7:
             self.vel = 7
-        if self.rect.y < 320:
+        if self.rect.y < 500:
             self.rect.y += int(self.vel)
         if self.vel == 0:
             self.flap = False
 
         # User Input
-        if pygame.mouse.get_pressed()[0] and not self.flap and self.rect.y > 0:
+        if pygame.mouse.get_pressed()[0] and not self.flap and self.rect.y > 0 and self.alive:
             self.flap = True
             self.vel = -7
             
 class Pipe(pygame.sprite.Sprite):
-    def __init__(self, x, y, image): # takes coordinates of pipe, image
+    def __init__(self, x, y, image, pipe_type): # takes coordinates of pipe, image
         pygame.sprite.Sprite.__init__(self) #initialize parent class
         self.image = image # = to image we are passing in agrs
         # conveinent for checking for collisions 
         self.rect = self.image.get_rect() # manipulate position of img
         self.rect.x, self.rect.y = x, y # set xy coords of img = to the xy coords we pass in agrs
+        self.enter, self.exit, self.passed = False, False, False
+        self.pipe_type = pipe_type
         
     def update(self): # responsible for moving pipes from left to right side of screen
         # move pipes
         self.rect.x -= scroll_speed
         if self.rect.x <= -SCREEN_WIDTH:
             self.kill()
-
+        
+        global score 
+        if self.pipe_type == "bottom":
+            if bird_start_position[0] > self.rect.topleft[0] and not self.passed:
+                self.enter = True 
+            if bird_start_position[0] > self.rect.topright[0] and not self.passed:
+                self.exit = True
+            if self.enter and self.exit and not self.passed:
+                self.passed = True
+                score += 1
+                
 class Ground(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -101,8 +117,10 @@ def quit_game():
 
 # Game Main Method
 def main():
+    global score
+
     # Instantiate Initial Ground
-    x_pos_ground, y_pos_ground = 0, 280
+    x_pos_ground, y_pos_ground = 0, 520
     ground = pygame.sprite.Group()
     ground.add(Ground(x_pos_ground, y_pos_ground))
     
@@ -110,9 +128,9 @@ def main():
     pipe_timer = 0
     pipes = pygame.sprite.Group()
     
-    # Instantiate Pony
-    pony = pygame.sprite.GroupSingle()
-    pony.add(Pony())
+    # Instantiate Bird
+    bird = pygame.sprite.GroupSingle()
+    bird.add(Bird())
 
     run = True
     while run:
@@ -121,7 +139,10 @@ def main():
 
         # Reset Frame
         screen.fill(BLACK)
-
+        
+        # User Input 
+        user_input = pygame.mouse.get_pressed()
+        
         # Draw Background
         screen.blit(skyline_image, (0, 0))
 
@@ -129,33 +150,69 @@ def main():
         if len(ground) <= 2:
             ground.add(Ground(SCREEN_WIDTH, y_pos_ground))
             
-        # Spawn Pipes 
-        if pipe_timer <= 0:
-            x_top, x_bottom = 479, 479
-            y_top = random.randint(-800, -600)
-            gap = random.randint(100, 130)
-            y_bottom = y_top + gap + bottom_pipe_image.get_height()
-            pipes.add(Pipe(x_top, y_top, top_pipe_image))
-            pipes.add(Pipe(x_bottom, y_bottom, bottom_pipe_image))
-            pipe_timer = random.randint(180, 250)
-        pipe_timer -= 1
-        
-        # Draw - Pipes, Ground, and Pony
+        # Draw - Pipes, Ground, and Bird
         pipes.draw(screen)
         ground.draw(screen)
-        pony.draw(screen)
+        bird.draw(screen)
         
-        # Update - Pipes, Ground, and Pony
-        pipes.update()
-        ground.update()
-        pony.update()
+        # Show Score
+        score_text = font.render ('Score: ' + str(score), True, pygame.Color(255, 255, 255))
+        screen.blit(score_text, (20, 20))
+        
+        # Update - Pipes, Ground, and Bird
+        if bird.sprite.alive:
+            pipes.update()
+            ground.update()
+            bird.update()
+        
+        # Pipe Collisions 
+        collision_pipes = pygame.sprite.spritecollide(bird.sprites()[0], pipes, False)
+        collision_ground = pygame.sprite.spritecollide(bird.sprites()[0], ground, False)
+        if collision_pipes or collision_ground:
+            bird.sprite.alive = False
+            screen.blit(game_over_image, (SCREEN_WIDTH // 2 - game_over_image.get_width() // 2, 
+                                              SCREEN_HEIGHT // 2 - game_over_image.get_height() // 2))
+            if pygame.mouse.get_pressed()[0]:
+                score = 0
+                break
+            
+        # Spawn Pipes 
+        if pipe_timer <= 0 and bird.sprite.alive:
+            x_top, x_bottom = 550, 550
+            y_top = random.randint(-600, -480)
+            gap = random.randint(90, 130)
+            y_bottom = y_top + gap + bottom_pipe_image.get_height()
+            pipes.add(Pipe(x_top, y_top, top_pipe_image, 'top'))
+            pipes.add(Pipe(x_bottom, y_bottom, bottom_pipe_image, 'bottom'))
+            pipe_timer = random.randint(180, 250)
+        pipe_timer -= 1
 
         clock.tick(60)
         pygame.display.update()
 
-    global score
-    # Show Score
-    score_text = font.render ('Score: ' + str(score), True, pygame.Color(255, 255, 255))
-    window.blit(score_text, (20, 20))
+def menu():
+    global game_stopped
+    
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            # Trigger the game start on a discrete mouse click
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                waiting = False
+                
+        # Draw Menu pop up
+        screen.fill((0, 0, 0))
+        screen.blit(skyline_image, (0, 0))
+        screen.blit(ground_image, Ground(0, 520))
+        screen.blit(bird_images[0], (100, 250))
+        screen.blit(start_image, (SCREEN_WIDTH // 10 - start_image.get_width() // 10,
+                                  SCREEN_WIDTH // 10 - start_image.get_height() // 10))
 
-main()
+        pygame.display.update()
+    main()
+
+while True:
+    menu()
